fcd0685783e4ef2e0799106033b64c9560b78b0b
root <root@gmail.com>
2018-04-05 11:57:22 +0530

    hello

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello Linux World\n This is Asif from Ducat");
+		System.out.println("Hello Linux World\n This is Asif from Ducat\nIts a new infromation for me . ");
 	}
 }
