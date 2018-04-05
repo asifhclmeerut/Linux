00617ad375af4650ef49d6ce46bad7aa9b0eb374
root <root@gmail.com>
2018-04-05 12:14:25 +0530

    new codes

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello Linux World\n This is Asif from Ducat\nIts a new infromation for me . ");
+		System.out.println("Hello Linux World\n This is Asif from Ducat\nIts a new infromation for me .\nI am learning Jenkins tool ");
 	}
 }
