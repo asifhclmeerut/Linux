78f7428cdda01cda9429a2be8b228b740007a535
root <root@gmail.com>
2018-04-08 14:19:36 +0530

    new

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
 public class HelloWorld
 {
 	public static void main(String[] args) {
-		System.out.println("Hello Linux World\n This is Asif from Ducat\n");
+		System.out.println("Hello Linux World\n This is Asif from Ducat\nI am not resposible for manage batches");
 	}
 }
